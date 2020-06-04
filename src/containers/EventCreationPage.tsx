6d6e067ae4ed34b7ecda1340836b742660a0c069
Jason Guo <jason.guo1@uwaterloo.ca>
2020-06-04 17:32:24 -0400

    remove extraneous import

--- src/containers/EventCreationPage.tsx
+++ src/containers/EventCreationPage.tsx
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Modal from "@material-ui/core/Modal";
 import CancelModal from "../components/EventCreationPage/CancelModal";
 
 const EventCreationPage = () => {
