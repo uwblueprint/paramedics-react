8d91f7c54328e38b9a4103fa11039c123b97db30
Jason Guo <jason.guo1@uwaterloo.ca>
2020-06-07 04:15:10 -0400

    slightly change dot styling

--- src/components/EventCreationPage/Stepper.tsx
+++ src/components/EventCreationPage/Stepper.tsx
@@ -1,33 +1,54 @@
-import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import MobileStepper from "@material-ui/core/MobileStepper";
 
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 400,
-        flexGrow: 1,
-    },
-});
+export default function Stepper({
+  activeStep,
+  nextButton,
+  backButton,
+}: {
+  activeStep: number;
+  nextButton: React.ReactNode;
+  backButton?: React.ReactNode;
+}) {
+  const classes = useStepperStyles();
+  const theme = useTheme();
 
-export default function Stepper({ activeStep, nextButton, backButton }: { activeStep: number, nextButton: React.ReactNode, backButton?: React.ReactNode }) {
-    const classes = useStyles();
-    const theme = useTheme();
-
-    return (
-        <MobileStepper
-            variant="dots"
-            steps={2}
-            position="static"
-            activeStep={activeStep}
-            className={classes.root}
-            nextButton={
-
-                nextButton}
-            backButton={
-                backButton
-            }
-        />)
+  return (
+    <MobileStepper
+      variant="dots"
+      steps={2}
+      position="static"
+      activeStep={activeStep}
+      classes={{
+        root: classes.root,
+        dots: classes.dots,
+        dot: classes.dot,
+        dotActive: classes.dotActive,
+      }}
+      nextButton={nextButton}
+      backButton={backButton}
+    />
+  );
 }
+
+const useStepperStyles = makeStyles({
+  root: {
+    maxWidth: 400,
+    flexGrow: 1,
+  },
+  dots: {
+    position: "absolute",
+    left: "4rem",
+    bottom: "4rem",
+  },
+  dot: {
+    width: "1rem",
+    height: "1rem",
+    backgroundColor: "#FFFFFF",
+    border: "3.6369px solid #05162F",
+  },
+  dotActive: {
+    backgroundColor: "#05162F",
+  },
+});
