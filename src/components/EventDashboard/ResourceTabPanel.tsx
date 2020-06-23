fb2f6cd581b835a0cb79c2eaf7a03a46ef6c5787
wongcmichelle <Michelle@Michelles-MacBook-Pro-2.local>
2020-06-23 18:24:16 -0400

    Add sorting and select boxes to resource tables

--- src/components/EventDashboard/ResourceTabPanel.tsx
+++ src/components/EventDashboard/ResourceTabPanel.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Box from '@material-ui/core/Box';
-import { makeStyles } from "@material-ui/core/styles";
-import { Colors } from '../../styles/Constants';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
 import { TabOptions } from './EventDashboardPage';
-import { useQuery } from '@apollo/react-hooks';
+import { Colors } from '../../styles/Constants';
+import { useQuery } from 'react-apollo';
 import { GET_ALL_HOSPITALS } from '../../graphql/queries/hospitals';
 import { GET_ALL_AMBULANCES } from '../../graphql/queries/ambulances';
-import { Typography, Button } from '@material-ui/core';
+import { Box, Typography, Button, Checkbox } from '@material-ui/core';
 import { FiberManualRecord, Add, Remove } from '@material-ui/icons';
 
 const useStyles = makeStyles({
@@ -21,7 +22,9 @@ const useStyles = makeStyles({
     paddingRight: '56px',
   },
   tableContainer: {
-    background: Colors.White
+    background: Colors.White,
+    border: `1px solid ${Colors.BorderLightGray}`,
+    borderRadius: '4px'
   },
   active: {
     color: Colors.ActiveGreen
@@ -35,8 +38,32 @@ const useStyles = makeStyles({
   },
   buttonIcon: {
     marginRight: '13px'
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+  toolbar: {
+    padding: '0 0 0 4px'
   }
 });
+
+interface Hospital {
+  id: string;
+  name: string;
+}
+
+interface Ambulance {
+  id: string;
+  vehicleNumber: number;
+}
 
 interface ResourceTabPanelProps {
   eventId: string;
@@ -45,86 +72,262 @@ interface ResourceTabPanelProps {
   ambulances?: Ambulance[];
 }
 
-interface Hospital {
-  id: number;
-  name: string;
+interface HospitalData extends Hospital {
+  isActive: boolean;
 }
 
-interface Ambulance {
-  id: number;
-  vehicleNumber: number;
+interface AmbulanceData extends Ambulance {
+  isActive: boolean;
 }
+
+const descendingComparator = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+const getComparator = (
+  order: Order,
+  orderBy,
+) => {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  headerId: string;
+  label: string;
+}
+
+interface EnhancedTableProps {
+  classes: ReturnType<typeof useStyles>;
+  onRequestSort: (event: React.MouseEvent, property: string) => void;
+  order: Order;
+  orderBy: string;
+  type: TabOptions.Hospital | TabOptions.Ambulance;
+}
+
+const EnhancedTableHead = (props: EnhancedTableProps) => {
+  const { classes, order, orderBy, onRequestSort, type } = props;
+  const createSortHandler = (property: string) => (event: React.MouseEvent) => {
+    onRequestSort(event, property);
+  };
+  const hospitalHeadCells = [
+    { headerId: 'name', label: 'Hospital Name' },
+    { headerId: 'isActive', label: 'Activity' },
+  ];
+  
+  const ambulanceHeadCells = [
+    { headerId: 'vehicleNumber', label: 'Digit' },
+    { headerId: 'isActive', label: 'Activity' },
+  ];
+  
+  const headCells: HeadCell[] = type === TabOptions.Hospital ? hospitalHeadCells : ambulanceHeadCells;
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+        </TableCell>
+        {headCells.map((headCell, index) => (
+          <TableCell
+            key={headCell.headerId}
+            align={index === 0 ? 'left' : 'right'}
+            sortDirection={orderBy === headCell.headerId ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.headerId}
+              direction={orderBy === headCell.headerId ? order : 'asc'}
+              onClick={createSortHandler(headCell.headerId)}
+            >
+              {headCell.label}
+              {orderBy === headCell.headerId ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+        <TableCell></TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  rowCount: number;
+}
+
+const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const { numSelected, onSelectAllClick, rowCount } = props;
+  const classes = useStyles();
+
+  return (
+    <Toolbar className={classes.toolbar} disableGutters>
+      <Checkbox
+        indeterminate={numSelected > 0 && numSelected < rowCount}
+        checked={rowCount > 0 && numSelected === rowCount}
+        onChange={onSelectAllClick}
+      />
+      {numSelected > 0 && (
+        <Typography variant="body2" component="div">
+          {numSelected} selected
+        </Typography>
+      )}
+      {numSelected > 0 && (
+        <Box display="flex" justifyContent="flex-end" flexGrow={1}>
+          <Button color="secondary"><Remove className={classes.buttonIcon}/>Exclude</Button>
+          <Button color="secondary"><Add className={classes.buttonIcon}/>Include</Button>
+        </Box>
+      )}
+    </Toolbar>
+  );
+};
 
 const ResourceTabPanel = ({eventId, type, hospitals = [], ambulances = []}: ResourceTabPanelProps) => {
   const classes = useStyles();
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<string>(type === TabOptions.Hospital ? 'name' : 'vehicleNumber');
+  const [selected, setSelected] = React.useState<string[]>([]);
+  
+  const createHospitalData = (
+    allHospitals: Hospital[]
+  ): HospitalData[] => {
+    return allHospitals.map(h => ({ id: h.id, name: h.name, isActive: hospitals.filter(hospital => hospital.id === h.id).length > 0 })); 
+  }
+  
+  const createAmbulanceData = (
+    allAmbulances: Ambulance[]
+  ): AmbulanceData[] => {
+    return allAmbulances.map(a => ({ id: a.id, vehicleNumber: a.vehicleNumber, isActive: ambulances.filter(ambulance => ambulance.id === a.id).length > 0 })); 
+  }
 
   const query = type === TabOptions.Hospital ? GET_ALL_HOSPITALS : GET_ALL_AMBULANCES;
 
   const { data } = useQuery(query);
 
-  const rows = data ? ( data.hospitals || data.ambulances ) : [];
+  const rows = data ? ( type === TabOptions.Hospital ? createHospitalData(data.hospitals) : createAmbulanceData(data.ambulances) ) : [];
+
+  const handleRequestSort = (event: React.MouseEvent, property: string) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = (rows as (HospitalData | AmbulanceData)[]).map((n) => n.id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const isSelected = (id: string) => selected.indexOf(id) !== -1;
+
+  const tableRows = stableSort(rows as (HospitalData | AmbulanceData)[], getComparator(order, orderBy))
+  .map((row: HospitalData | AmbulanceData, index) => {
+    const isItemSelected = isSelected(row.id);
+    const labelId = `enhanced-table-checkbox-${index}`;
+
+    return (
+      <TableRow
+        hover
+        role="checkbox"
+        aria-checked={isItemSelected}
+        tabIndex={-1}
+        key={row.id}
+        selected={isItemSelected}
+      >
+        <TableCell padding="checkbox">
+          <Checkbox
+            checked={isItemSelected}
+            inputProps={{ 'aria-labelledby': labelId }}
+            onClick={(event) => handleClick(event, row.id)}
+          />
+        </TableCell>
+        <TableCell component="th" id={labelId} scope="row">
+          {type === TabOptions.Hospital ? (row as HospitalData).name : (row as AmbulanceData).vehicleNumber}
+        </TableCell>
+        <TableCell align="right">
+        {row.isActive ? 
+          <Typography variant="button" className={classes.active}><FiberManualRecord className={classes.activeIcon}/>Active</Typography> : 
+          <Typography variant="button" className={classes.inactive}>Inactive</Typography>
+        }
+        </TableCell>
+        <TableCell align="right">
+          {row.isActive ? 
+            <Button color="secondary" disabled={selected.length > 0}><Remove className={classes.buttonIcon}/>Exclude</Button> : 
+            <Button color="secondary" disabled={selected.length > 0}><Add className={classes.buttonIcon}/>Include</Button>
+          }
+        </TableCell>
+      </TableRow>
+    );
+  });
 
   return (
-    <Box className={classes.root}>
-      <TableContainer className={classes.tableContainer}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>{type === TabOptions.Hospital ? 'Hospital Name' : 'Digit'}</TableCell>
-              <TableCell align="right">Activity</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {type === TabOptions.Hospital && rows.map((row:Hospital) => {
-                const isActive = hospitals.filter(h => h.id === row.id).length > 0;
-                return (
-                  <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">
-                      {isActive ? 
-                        <Typography variant="button" className={classes.active}><FiberManualRecord className={classes.activeIcon}/>Active</Typography> : 
-                        <Typography variant="button" className={classes.inactive}>Inactive</Typography>
-                      }
-                    </TableCell>
-                    <TableCell align="right">
-                      {isActive ? 
-                        <Button color="secondary"><Remove className={classes.buttonIcon}/>Exclude</Button> : 
-                        <Button color="secondary"><Add className={classes.buttonIcon}/>Include</Button>
-                      }
-                    </TableCell>
-                  </TableRow> 
-                )
-            })}
-            {type === TabOptions.Ambulance && rows.map((row:Ambulance) => {
-              const isActive = ambulances.filter(a => a.id === row.id).length > 0;
-              return (
-                <TableRow key={row.vehicleNumber}>
-                <TableCell component="th" scope="row">
-                  {row.vehicleNumber}
-                </TableCell>
-                <TableCell align="right">
-                  {isActive ? 
-                    <Typography variant="button" className={classes.active}><FiberManualRecord className={classes.activeIcon}/>Active</Typography> : 
-                    <Typography variant="button" className={classes.inactive}>Inactive</Typography>
-                  }
-                  </TableCell>
-                  <TableCell align="right">
-                    {isActive ? 
-                      <Button color="secondary"><Remove className={classes.buttonIcon}/>Exclude</Button> : 
-                      <Button color="secondary"><Add className={classes.buttonIcon}/>Include</Button>
-                    }
-                  </TableCell>
-              </TableRow>
-              )
-            })}            
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
-  )
+    <div className={classes.root}>
+        <EnhancedTableToolbar 
+          numSelected={selected.length}
+          onSelectAllClick={handleSelectAllClick}
+          rowCount={rows.length}
+        />
+        <TableContainer className={classes.tableContainer}>
+          <Table>
+            <EnhancedTableHead
+              type={type}
+              classes={classes}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {tableRows}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    </div>
+  );
 }
 
 export default ResourceTabPanel;
