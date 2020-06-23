9575b56da93fc4ac602103f8c3553406dca9cad5
wongcmichelle <Michelle@Michelles-MacBook-Pro-2.local>
2020-06-22 21:44:14 -0400

    Add include exclude buttons

--- src/components/EventDashboard/ResourceTabPanel.tsx
+++ src/components/EventDashboard/ResourceTabPanel.tsx
@@ -12,8 +12,8 @@ import { TabOptions } from './EventDashboardPage';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_ALL_HOSPITALS } from '../../graphql/queries/hospitals';
 import { GET_ALL_AMBULANCES } from '../../graphql/queries/ambulances';
-import { Typography } from '@material-ui/core';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import { Typography, Button } from '@material-ui/core';
+import { FiberManualRecord, Add, Remove } from '@material-ui/icons';
 
 const useStyles = makeStyles({
   root: {
@@ -32,6 +32,9 @@ const useStyles = makeStyles({
   },
   inactive: {
     color: Colors.InactiveGrey
+  },
+  buttonIcon: {
+    marginRight: '13px'
   }
 });
 
@@ -69,35 +72,54 @@ const ResourceTabPanel = ({eventId, type, hospitals = [], ambulances = []}: Reso
             <TableRow>
               <TableCell>{type === TabOptions.Hospital ? 'Hospital Name' : 'Digit'}</TableCell>
               <TableCell align="right">Activity</TableCell>
+              <TableCell></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {type === TabOptions.Hospital && rows.map((row:Hospital) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">
-                  {(hospitals.filter(h => h.id === row.id).length > 0) ? 
-                    <Typography variant="button" className={classes.active}><FiberManualRecordIcon className={classes.activeIcon}/>Active</Typography> : 
-                    <Typography variant="button" className={classes.inactive}>Inactive</Typography>
-                  }
-                </TableCell>
-              </TableRow>
-            ))}
-            {type === TabOptions.Ambulance && rows.map((row:Ambulance) => (
-              <TableRow key={row.vehicleNumber}>
+            {type === TabOptions.Hospital && rows.map((row:Hospital) => {
+                const isActive = hospitals.filter(h => h.id === row.id).length > 0;
+                return (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">
+                      {isActive ? 
+                        <Typography variant="button" className={classes.active}><FiberManualRecord className={classes.activeIcon}/>Active</Typography> : 
+                        <Typography variant="button" className={classes.inactive}>Inactive</Typography>
+                      }
+                    </TableCell>
+                    <TableCell align="right">
+                      {isActive ? 
+                        <Button color="secondary"><Remove className={classes.buttonIcon}/>Exclude</Button> : 
+                        <Button color="secondary"><Add className={classes.buttonIcon}/>Include</Button>
+                      }
+                    </TableCell>
+                  </TableRow> 
+                )
+            })}
+            {type === TabOptions.Ambulance && rows.map((row:Ambulance) => {
+              const isActive = ambulances.filter(a => a.id === row.id).length > 0;
+              return (
+                <TableRow key={row.vehicleNumber}>
                 <TableCell component="th" scope="row">
                   {row.vehicleNumber}
                 </TableCell>
                 <TableCell align="right">
-                  {(ambulances.filter(a => a.id === row.id).length > 0) ? 
-                    <Typography variant="button" className={classes.active}><FiberManualRecordIcon className={classes.activeIcon}/>Active</Typography> : 
+                  {isActive ? 
+                    <Typography variant="button" className={classes.active}><FiberManualRecord className={classes.activeIcon}/>Active</Typography> : 
                     <Typography variant="button" className={classes.inactive}>Inactive</Typography>
                   }
                   </TableCell>
+                  <TableCell align="right">
+                      {isActive ? 
+                        <Button color="secondary"><Remove className={classes.buttonIcon}/>Exclude</Button> : 
+                        <Button color="secondary"><Add className={classes.buttonIcon}/>Include</Button>
+                      }
+                    </TableCell>
               </TableRow>
-            ))}            
+              )
+            })}            
           </TableBody>
         </Table>
       </TableContainer>
