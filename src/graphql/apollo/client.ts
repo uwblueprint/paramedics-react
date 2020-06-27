e990ea1fc72b8c3b64199c013a65dd5937f150f7
Ricky Mao <rickyrmao@gmail.com>
2020-06-27 11:39:43 -0700

    Added .env to gitignore

--- src/graphql/apollo/client.ts
+++ src/graphql/apollo/client.ts
@@ -1,6 +1,7 @@
 import ApolloClient from "apollo-boost";
 import defaults from "./defaults";
 
+
 const client = new ApolloClient({
   uri: process.env.REACT_APP_BACKEND_HOST || "http://localhost:4000/",
   clientState: {
