f25fee31dd298d51321d77bdb7e4186104bafcd5
Daniel <daniel.chen115@gmail.com>
2020-06-28 12:13:13 -0400

    begin implement table sorting

--- src/components/EventDashboard/CCPTabPanel.tsx
+++ src/components/EventDashboard/CCPTabPanel.tsx
@@ -5,11 +5,14 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from "@material-ui/core/styles";
 import { Colors } from '../../styles/Constants';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_ALL_CCPS } from '../../graphql/queries/templates/ccps';
+
+type Order = 'asc' | 'desc';
 
 const useStyles = makeStyles({
   root: {
@@ -18,32 +21,118 @@ const useStyles = makeStyles({
   },
   tableContainer: {
     background: Colors.White
-  }
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
 });
 
 interface CCP {
   id: string;
   name: string;
+  address: string; // address is not implemented
   eventId: { id: string };
+}
+
+const stableSort = (array: string[], comparator: (a: string, b: string) => number) => {
+  const stabilizedThis = array.map((el, index) => [el, index] as [string, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof CCP;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: HeadCell[] = [
+  { id: 'name', numeric: false, disablePadding: true, label: 'CCP Name' },
+  { id: 'address', numeric: true, disablePadding: false, label: 'Address' },
+]
+
+interface EnhancedTableProps {
+classes: ReturnType<typeof useStyles>;
+onRequestSort: (event: React.MouseEvent, property: keyof CCP) => void;
+order: Order;
+orderBy: string;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property: keyof CCP) => (event: React.MouseEvent) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
 }
 
 const CCPTabPanel = ({eventId}: {eventId: string}) => {
   const classes = useStyles();
 
   const { loading, error, data } = useQuery(GET_ALL_CCPS);
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof CCP>('name');
 
   const rows = data ? data.collectionPoints.filter((ccp:CCP) => ccp.eventId.id === eventId) : [];
+
+  const handleRequestSort = (event: React.MouseEvent, property: keyof CCP) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
   return (
     <Box className={classes.root}>
       <TableContainer className={classes.tableContainer}>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>CCP Name</TableCell>
-              <TableCell align="right">Address</TableCell>
-            </TableRow>
-          </TableHead>
+        <EnhancedTableHead
+              classes={classes}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
           <TableBody>
             {rows.map((row:CCP) => (
               <TableRow key={row.name}>
