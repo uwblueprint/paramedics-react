8fdf31adbba0ad6e0fbf61c5cfe20556e1be219e
wongcmichelle <Michelle@Michelles-MacBook-Pro-2.local>
2020-06-28 16:06:27 -0400

    Add custom material ui palette and typography

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,24 +1,22 @@
 import React from "react";
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from "../styles/Theme";
 import "../styles/App.css";
-
 import { Switch, Route, Redirect } from "react-router-dom";
 import HomeLandingPage from "./HomeLandingPage";
 import EventCreationPage from "./EventCreationPage";
 
 function App() {
   return (
-    <div>
-      <div>
-        <Switch>
-          <Route exact path="/events" component={HomeLandingPage} />
-          <Route exact path="/events/new" component={EventCreationPage} />
-
-          <Route path="/">
-            <Redirect to="/events" />
-          </Route>
-        </Switch>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Switch>
+      <Route exact path="/events" component={HomeLandingPage} />
+      <Route exact path="/events/new" component={EventCreationPage} />
+      <Route path="/">
+        <Redirect to="/events" />
+      </Route>
+    </Switch>
+    </ThemeProvider>
   );
 }
 
