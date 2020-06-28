5f2cb562924f2ffd6ae02798bf39e531b9828067
wongcmichelle <Michelle@Michelles-MacBook-Pro-2.local>
2020-06-28 16:07:59 -0400

    Add whitespace

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -9,13 +9,13 @@ import EventCreationPage from "./EventCreationPage";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Switch>
-      <Route exact path="/events" component={HomeLandingPage} />
-      <Route exact path="/events/new" component={EventCreationPage} />
-      <Route path="/">
-        <Redirect to="/events" />
-      </Route>
-    </Switch>
+      <Switch>
+        <Route exact path="/events" component={HomeLandingPage} />
+        <Route exact path="/events/new" component={EventCreationPage} />
+        <Route path="/">
+          <Redirect to="/events" />
+        </Route>
+      </Switch>
     </ThemeProvider>
   );
 }
