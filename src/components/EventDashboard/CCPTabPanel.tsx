9eee3a0d0e9ec268a05f0bb0e40c2af080c1ea1a
Daniel <daniel.chen115@gmail.com>
2020-06-28 12:44:12 -0400

    add compare functions [WIP]

--- src/components/EventDashboard/CCPTabPanel.tsx
+++ src/components/EventDashboard/CCPTabPanel.tsx
@@ -42,9 +42,30 @@ interface CCP {
   eventId: { id: string };
 }
 
-const stableSort = (array: string[], comparator: (a: string, b: string) => number) => {
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+const stableSort = (array: string[], comparator: (a: CCP, b: CCP) => number) => {
   const stabilizedThis = array.map((el, index) => [el, index] as [string, number]);
   stabilizedThis.sort((a, b) => {
+    console.log(a);
+    console.log(b);
     const order = comparator(a[0], b[0]);
     if (order !== 0) return order;
     return a[1] - b[1];
@@ -134,14 +155,18 @@ const CCPTabPanel = ({eventId}: {eventId: string}) => {
               onRequestSort={handleRequestSort}
             />
           <TableBody>
-            {rows.map((row:CCP) => (
-              <TableRow key={row.name}>
+          {stableSort(rows, getComparator(order, orderBy))
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+                  return (
+                    <TableRow key={row.name}>
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
                 <TableCell align="right">-</TableCell>
               </TableRow>
-            ))}
+                );
+          })};
           </TableBody>
         </Table>
       </TableContainer>
