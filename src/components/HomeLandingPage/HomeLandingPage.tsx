3cb3f5f611428ec31d2bfa5c46b17e382d6aa017
Michelle Wong <wong.cmichelle@gmail.com>
2020-08-17 03:07:47 +0000

    Remove unused useEffect

--- src/components/HomeLandingPage/HomeLandingPage.tsx
+++ src/components/HomeLandingPage/HomeLandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-apollo';
 import { Grid, Typography } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
