8f4d5d3cdc3951ca65bf39465d5673e53f3a3812
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-10-27 21:25:15 -0400

    ws support added

--- src/graphql/apollo/client.ts
+++ src/graphql/apollo/client.ts
@@ -2,7 +2,34 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const wsLink: ApolloLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink: ApolloLink = new HttpLink({
+  uri: process.env.REACT_APP_BACKEND_HOST || 'http://localhost:4000/',
+  credentials: 'same-origin',
+});
+
+const link: ApolloLink = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
   link: ApolloLink.from([
@@ -17,10 +44,7 @@ const client = new ApolloClient({
       // eslint-disable-next-line no-console
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
-    new HttpLink({
-      uri: process.env.REACT_APP_BACKEND_HOST || 'http://localhost:4000/',
-      credentials: 'same-origin',
-    }),
+    link,
   ]),
   cache: new InMemoryCache(),
 });
