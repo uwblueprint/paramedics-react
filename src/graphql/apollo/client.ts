438e9b50fe7bd8761f91713770aa0dce129429d9
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-10-27 21:15:12 -0400

    changed to apollo client

--- src/graphql/apollo/client.ts
+++ src/graphql/apollo/client.ts
@@ -1,11 +1,28 @@
-import ApolloClient from 'apollo-boost';
-import defaults from './defaults';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_BACKEND_HOST || 'http://localhost:4000/',
-  clientState: {
-    defaults,
-  },
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          // eslint-disable-next-line no-console
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      // eslint-disable-next-line no-console
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.REACT_APP_BACKEND_HOST || 'http://localhost:4000/',
+      credentials: 'same-origin',
+    }),
+  ]),
+  cache: new InMemoryCache(),
 });
 
 export default client;
