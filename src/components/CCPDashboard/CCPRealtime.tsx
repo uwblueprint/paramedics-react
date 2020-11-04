53ebb5772baff9d325b5a61a1bf764263a51a945
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-11-03 22:05:17 -0500

    number of updates shown

--- src/components/CCPDashboard/CCPRealtime.tsx
+++ src/components/CCPDashboard/CCPRealtime.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSubscription } from '@apollo/react-hooks';
 import { PATIENTS_SUBSCRIPTION } from '../../graphql/subscriptions/patients';
 
@@ -7,16 +7,23 @@ interface Props {
 }
 
 export const CCPRealtime = (props: Props) => {
+  const [numUpdates, setNumUpdates] = useState(0);
+  const [listUpdates, setListUpdates] = useState([]);
   const { collectionPointId } = props;
   const { data, loading } = useSubscription(PATIENTS_SUBSCRIPTION, {
     variables: { collectionPointId },
+    onSubscriptionData: ({ subscriptionData: { data } }) => {
+      setNumUpdates(numUpdates + 1);
+      //setListUpdates([...listUpdates, data]);
+    },
   });
 
   const id: string = data ? data.patientUpdated.id : '';
 
   return (
     <div>
-      <p>{loading ? 'Updates Loading...' : id}</p>
+      <p>new updates: {numUpdates}</p>
+      <p>{loading ? '' : 'patient ' + id + ' updated'}</p>
     </div>
   );
 };
