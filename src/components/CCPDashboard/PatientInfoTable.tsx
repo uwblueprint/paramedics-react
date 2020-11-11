5116ce7515e64e7b00c6a3c41d556a03aa9b8f33
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-11-10 22:43:15 -0500

    clean-up

--- src/components/CCPDashboard/PatientInfoTable.tsx
+++ src/components/CCPDashboard/PatientInfoTable.tsx
@@ -216,8 +216,6 @@ export const PatientInfoTable = ({
     setOpenDeletePatient(false);
   };
 
-  React.useEffect(() => {}, [patients]);
-
   const tableRows = stableSort(patients, getComparator(order, orderBy)).map(
     (patient: Patient) => {
       const triageLevels = {
