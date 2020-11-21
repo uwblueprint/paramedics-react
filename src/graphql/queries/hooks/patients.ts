ffb61da9862cb0ba3e1f48e2ae4421aab7cb5ed8
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-11-20 23:31:20 -0500

    whole application is now realtime!!!

--- src/graphql/queries/hooks/patients.ts
+++ src/graphql/queries/hooks/patients.ts
@@ -1,11 +1,15 @@
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { useApolloClient } from 'react-apollo';
 import { GET_ALL_PATIENTS } from '../patients';
 
-export function useAllPatients() {
-  const { data } = useQuery(GET_ALL_PATIENTS);
+export function useAllPatients(eventId?: string) {
+  const { data, refetch } = useQuery(GET_ALL_PATIENTS);
   const client = useApolloClient();
 
+  React.useEffect(() => {
+    refetch();
+  }, [eventId]);
   if (data) {
     client.writeQuery({
       query: GET_ALL_PATIENTS,
