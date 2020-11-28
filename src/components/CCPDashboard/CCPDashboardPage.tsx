92c8f63a6eb66993d4c865de5a45fd7d7f43697f
Carelynn Tsai <carelynntsai@gmail.com>
2020-11-28 15:21:15 -0500

    address PR comments

--- src/components/CCPDashboard/CCPDashboardPage.tsx
+++ src/components/CCPDashboard/CCPDashboardPage.tsx
@@ -124,7 +124,7 @@ const TabPanel = (props: TabPanelProps) => {
 
 const CCPDashboardPage = ({ match }: RouteComponentProps<TParams>) => {
   const classes = useStyles();
-
+  const highlightDuration = 5000; // seconds
   const { eventId, ccpId, patientId } = match.params;
   const location = useLocation<LocationState>();
   const { userUpdatedPatientId } = location.state || {
@@ -168,7 +168,7 @@ const CCPDashboardPage = ({ match }: RouteComponentProps<TParams>) => {
     setLastUpdatedPatient(id);
     const highlightTimeout = setTimeout(() => {
       setLastUpdatedPatient('');
-    }, 5000);
+    }, highlightDuration);
     setLastUpdatedPatientTimeout(highlightTimeout);
   };
 
